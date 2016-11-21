2ca4466dfa312ca706db9a1aeadcba17f61a7345
William Pring <wp@williampring.com>
2016-11-21 13:30:58 -0500

    Add files via upload
    Changed updated with option to change your browser and made it more generic for usage MVC note

--- gulpfile with browserSync.js
+++ gulpfile with browserSync.js
@@ -1,17 +1,37 @@
 var gulp = require('gulp');
-var browserSync = require('browser-sync');
+var browserSync = require('browser-sync').create();
+
+
+
+
+gulp.task('default', function () {
+    console.log('Gulp is now running.....');
+    console.log('Gulp is now tracking changes(HTML, CSS, JS)');
+});
 
 gulp.task('watch', function () {
+    browserSync.init({
+        //Change your url base on your localhost
+        proxy: "",
+        //change to google chrome if os is Windows 7+
+        browser: "chrome"
+    });
+    /*
+    ************** OR ****************
+    browserSync.init({
+        server: {
+            //init server name
+            baseDir:'./'
+        }
+    });
+    ************* END **************
+    */
+
+
     function reportChange(event) {
         console.log('Event type: ' + event.type); // added, changed, or deleted
         console.log('Event path: ' + event.path); // The path of the modified file
     }
+    //add additional watch change path if you wish
     gulp.watch('Content/*.css', [browserSync.reload]).on('change', reportChange);
-    gulp.watch('Scripts/*.js', [browserSync.reload]).on('change', reportChange);
-    gulp.watch('Views/Master/*.cshtml', [browserSync.reload]).on('change', reportChange);
-
-});
-
-gulp.task('default', function () {
-    console.log('Gulp is now running.....');
 });
