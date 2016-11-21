a11ae3d2046fc18d0c9e394b64e45f16a0aa4120
William Pring <wp@williampring.com>
2016-11-21 11:33:33 -0500

    Add files via upload
    Added a new gulp file that will sync the code live to your browser. For mainly ASP.NET is what I used it for

---
+++ gulpfile with browserSync.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+
+gulp.task('watch', function () {
+    function reportChange(event) {
+        console.log('Event type: ' + event.type); // added, changed, or deleted
+        console.log('Event path: ' + event.path); // The path of the modified file
+    }
+    gulp.watch('Content/*.css', [browserSync.reload]).on('change', reportChange);
+    gulp.watch('Scripts/*.js', [browserSync.reload]).on('change', reportChange);
+});
+
+gulp.task('default', function () {
+    console.log('Gulp is now running.....');
+});
